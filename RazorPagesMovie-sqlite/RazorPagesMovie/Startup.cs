90a9fc1a302ae7c8b0b05393f789366639093f0c
Sayed Hashimi <sayedha@microsoft.com>
2020-06-12 02:55:26 +0000

    before scaffolding via cli into sqlite project

--- RazorPagesMovie-sqlite/RazorPagesMovie/Startup.cs
+++ RazorPagesMovie-sqlite/RazorPagesMovie/Startup.cs
@@ -33,6 +33,8 @@ namespace RazorPagesMovie
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddRazorPages();
+            services.AddDbContext<RazorPagesMovieContext>(options =>
+                        options.UseSqlite(Configuration.GetConnectionString("MovieContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
